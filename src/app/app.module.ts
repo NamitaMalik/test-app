ac8b35b49d2bfde00092a060956a11049492b1ba
NamitaMalik <namita1990@gmail.com>
2020-03-30 16:21:14 +0200

    Trying with child route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChildComponentComponent } from './child-component/child-component.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'child-route', component: ChildComponentComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+  RouterModule.forRoot(
+    appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
